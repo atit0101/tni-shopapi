e58eb149254dd16847fab494d1bc4a076f6f5506
อาทิตย์ ขาวเอี่ยม <atit@sMacBookPro.local>
2024-07-09 09:06:39 +0700

    up

--- obj/Debug/net8.0/server.AssemblyInfo.cs
+++ obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeed5c67532de12dc485f46f3b8ae9ceec657a04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25c8a2eae0be9ab5f82ca342e497a2bde9cbc671")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
